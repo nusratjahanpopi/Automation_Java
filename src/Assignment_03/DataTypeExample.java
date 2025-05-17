6f10dc7b686ba13b151d82e144f242b56adb2d4b
nusratjahanpopi <nusratjahan736821@gmail.com>
2025-05-17 15:33:58 +0600

    Java Assignment_02 Submitted

---
+++ src/Assignment_03/DataTypeExample.java
@@ -0,0 +1,39 @@
+package Assignment_03;
+
+public class DataTypeExample {
+    public static void main(String[] args) {
+
+        // Primitive data types
+
+        int num1 = 10;
+        long num2 = 100000000l;
+        double num3 = 1000.5567;
+        float num4 = 20087656.56678f;
+        String name = "Nusrat Jahan";
+        boolean isSomething = true;
+        System.out.println("Primitive data types");
+        System.out.println("int : " + num1);
+        System.out.println("long : " + num2);
+        System.out.println("double : " + num3);
+        System.out.println("float : " + num4);
+        System.out.println("string : " + name);
+        System.out.println("boolean : " + isSomething);
+
+
+
+        // Non-primitive data types
+
+        Long num5 = 100000000l;
+        Double Num6 = 1000.5567;
+        Float num7 = 20087656.56678f;
+        String name2 = "Nusrat Jahan";
+        Boolean isSomething2 = true;
+        Integer num8 = 100;
+        System.out.println("Non-primitive data types");
+        System.out.println("Long : " + num5);
+        System.out.println("Double : " + Num6);
+        System.out.println("Float : " + num7);
+        System.out.println("String : " + name2);
+        System.out.println("Boolean : " + isSomething2);
+}
+    }
---
+++ src/Assignment_03/Person.java
@@ -0,0 +1,28 @@
+package Assignment_03;
+
+public class Person {
+
+    // Variables fields
+    String name;
+    int age;
+
+    // Constructor
+    public Person(String PersonName, int PersonAge) {
+        name = PersonName;
+        age = PersonAge;
+    }
+
+    // method call
+    public void printInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+
+
+}
+
+
+
+
+
+
