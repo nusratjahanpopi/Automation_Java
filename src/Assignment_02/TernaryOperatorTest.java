502bbc5bd2eaec77da00418c6877bd7cebf87609
Nusrat Jahan <93383507+nusratjahanpopi@users.noreply.github.com>
2025-05-17 16:19:21 +0600

    Delete src/Assignment_02 directory

--- src/Assignment_02/TernaryOperatorTest.java
+++
@@ -1,9 +0,0 @@
-package Assignment_02;
-
-public class TernaryOperatorTest {
-    public static void main(String[] args) {
-        int num1 = 100, num2 = 200;
-        int max = (num1 > num2 ) ? num1 : num2;
-        System.out.println("Maximum number is : " + max);
-    }
-}
